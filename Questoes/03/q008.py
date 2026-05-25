7e25de16d62d4d6b59c2253c3d225d9447389b23
Joiran Costa <42392957+joirancosta@users.noreply.github.com>
2026-05-25 19:42:16 +0000

    fim q008

--- Questoes/03/q008.py
+++ Questoes/03/q008.py
@@ -1,6 +1,57 @@
 import random
+import os
+
+def exibe_mapa():
+  for linha in mapa_tesouro:
+    print('|'.join(linha))
+    print('-' * 7)
+
+mapa_tesouro = [
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+]
 
 linha_tesouro = random.randint(0, 3)
 coluna_tesouro = random.randint(0, 3)
+tentativas = 0
 
-print(f"Linha: '{linha_tesouro}'. Coluna: '{coluna_tesouro}'.")
+while True:
+  try:
+    print('Tente achar o tesouro no mapa!')
+    print(f"Tentativa {tentativas}")
+    exibe_mapa()
+
+    if tentativas >= 5:
+      print('Game Over, você perdeu!')
+      break
+
+    linha = int(input('Digite a linha: '))
+    coluna = int(input('Digite a coluna: '))
+
+    if mapa_tesouro[linha][coluna] == 'X':
+      print('Posição já jogada!')
+      continue
+    elif (
+        mapa_tesouro[linha][coluna] != ' ' or 
+        not 0 <= linha <= 3 or 
+        not 0 <= coluna <= 3
+      ):
+      print('Jogada inválida!')
+      continue
+    elif linha == linha_tesouro and coluna == coluna_tesouro:
+      os.system("clear")
+      print('Parabéns, você venceu! Achou o tesouro!')
+      mapa_tesouro[linha][coluna] = '🌟'
+      exibe_mapa()
+      break
+    else:
+      mapa_tesouro[linha][coluna] = 'X'
+      tentativas += 1
+    
+    os.system("clear")
+  except ValueError:
+    print('Digite valores númericos entre 0 e 2.')
+  except IndexError:
+    print('Digite valores númericos entre 0 e 2.')
